f919af97814dbe909845b450f93695a705583a51
bouquet1 <bukuleta@gmail.com>
2023-05-29 16:29:56 -0700

    navbar passes test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,19 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const NavLinks = links.map((link) => (
+    <a key={link} href={"#" + link}>
+      {link}
+    </a>
+  ));
+  return (
+    <nav>
+      {/* <a href="#home">home</a>
+      <a href="#about">about</a>
+      <a href="#projects">projects</a> */}
+      {NavLinks}
+    </nav>
+  );
 }
 
 export default NavBar;
