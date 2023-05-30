e0e5fb12bf39fb6f90997cfa3e38052ec5cdb0cb
bouquet1 <bukuleta@gmail.com>
2023-05-29 18:00:04 -0700

    Project Item doesn't pass

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  console.log(technologies);
   const technologiesSpan = technologies.map((technology) => <span technologies={technology.technologies}></span>);
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">{/* render a <span> for each technology in the technologies array */}</div>
+      <div className="technologies">
+        {/* render a <span> for each technology in the technologies array */}
+        {/* {technologiesSpan} */}
+      </div>
     </div>
   );
 }
