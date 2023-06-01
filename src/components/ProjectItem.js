74fe9e4cac56e163e77ff57186957e575c755db4
bouquet1 <bukuleta@gmail.com>
2023-06-01 08:23:08 -0700

    test passed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -2,16 +2,14 @@ import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
   console.log(technologies);
-  const technologiesSpan = technologies.map((technology, index) => <span technology={technology.index}></span>);
+
+  const technologiesSpan = technologies.map((technology, index) => <span key={index}>{technology}</span>);
 
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-        {/* {technologiesSpan} */}
-      </div>
+      <div className="technologies">{technologiesSpan}</div>
     </div>
   );
 }
